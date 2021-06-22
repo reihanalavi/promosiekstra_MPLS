352afa9fc83c182708716fe8b203b55da8f54ca5
reihanalavi <reihanalavi@users.noreply.github.com>
2021-06-22 20:55:17 +0700

    First Commit - check if the youtube player work

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1624369815,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/bg-sheet0.png",
+		"images/header-sheet0.png",
+		"images/ekstra_-sheet0.png",
+		"images/ekstra_-sheet1.png",
+		"images/ekstra_-sheet2.png",
+		"images/guide-sheet0.png",
+		"images/btnkanan_-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/layervideo-sheet0.png",
+		"images/btnkembali-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
