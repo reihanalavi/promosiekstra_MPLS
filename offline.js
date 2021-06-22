cc832f9815bcf6a309972be1ba63f0c5d2175379
reihanalavi <reihanalavi@users.noreply.github.com>
2021-06-22 21:31:29 +0700

    Third Commit - added controller for next and prev

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1624370753,
+	"version": 1624372225,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -17,6 +17,7 @@
 		"images/bg2-sheet0.png",
 		"images/layervideo-sheet0.png",
 		"images/btnkembali-sheet0.png",
+		"images/controller_button-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
